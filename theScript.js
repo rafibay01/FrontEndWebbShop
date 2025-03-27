9c443001011814ac4bbae5e13533cc1aefff996a
AdamRitzler <adam.ritzler@gmail.com>
2025-03-27 10:17:09 +0100

    Refactor product title and image loading logic to improve readability and maintainability

--- theScript.js
+++ theScript.js
@@ -1,21 +1,22 @@
 fetch('https://fakestoreapi.com/products')
-  .then(response => response.json())
-  .then(data => {
-    const titleElements = document.querySelectorAll('.product-title'); // Hämta alla h4 med klassen "product-title"
+    .then(response => response.json())
+    .then(data => {
+        const titleElements = document.querySelectorAll(`.product-title`);
 
-    titleElements.forEach((titleElement, index) => {
-      if (data[index]) {
-        titleElement.textContent = data[index].title; // Uppdatera titeln med produktens titel
-      }
-    });
+        titleElements.forEach((titleElement, index) => {
+            if (data[index]) {
+                titleElement.textContent = data[index].title;
+            }
+        });
 
-    data.forEach((product, index) => {
-      const imgElement = document.getElementById(`product${index + 1}`);
-      if (imgElement) {
-        imgElement.src = product.image; // Uppdatera bildens src-attribut
-      }
+        data.forEach((product, index) => {
+            const imgElement = document.getElementById(`product${index + 1}`);
+
+            if (imgElement) {
+                imgElement.src = product.image;
+            }
+        });
     });
-  });
 fetch('https://fakestoreapi.com/products')
     .then(data => console.log(data));
 
